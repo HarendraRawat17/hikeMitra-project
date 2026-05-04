b3c945381cd98ca038d53f090c64bb2fcdd2ab6d
HarendraRawat17 <yt2022a3@gmail.com>
2026-05-04 08:46:27 +0530

    Utils formation

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './index.css'
+
+function App() {
+  return (
+    <div> My Project </div>
+  )
+}
+
+export default App
--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,7 @@
 import dotenv from "dotenv";
-import express from "express";  
+import express, { urlencoded } from "express"; 
+import cors from "cors" ;
+import cookieParser from "cookie-parser";
 import connectDB from "./Database/index.js";    
 
 
@@ -8,3 +10,17 @@ dotenv.config()
 
 
 connectDB()
+
+const app = express();
+
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+  credentials: true
+}));
+
+app.use(express.json({limit: "15kb"}));
+app.use(express.urlencoded({extended: true, limit: "15kb"}));
+app.use(express.static("public")) // general config to store pdf, img files locally in server
+app.use(cookieParser());
+ 
+export {app};
